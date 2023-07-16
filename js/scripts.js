ad851f93104fe653c07773997be234af3b57c061
Sean Norwood <norwood.sean@gmail.com>
2023-07-16 10:13:40 -0500

    Add calculate cost code
    Signed-off-by: Sean Norwood <norwood.sean@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,18 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
+
+Pizza.prototype.calculatePrice = function () {
+  let basePrice = 0;
+
+  if (this.size === "small") {
+    basePrice = 5;
+  } else if (this.size === "medium") {
+    basePrice = 7;
+  } else {
+    basePrice = 9;
+  }
+
+  const toppingsPrice = this.toppings.length * 0.5;
+  return basePrice + toppingsPrice;
+};
